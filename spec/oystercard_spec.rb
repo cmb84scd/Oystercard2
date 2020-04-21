4cf49d8823b306499f7f42176ffa89b5010d6eb4
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-21 15:24:14 +0100

    Adds top_up method; passes test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,9 +3,23 @@ require 'oystercard'
 describe Oystercard do
 
   describe 'balance' do
+
+    it 'responds to balance' do
+      expect(subject).to respond_to(:balance)
+    end
   
     it 'balance has 0' do
       expect(subject.balance).to eq(0)
+    end
+
+  end
+  
+  describe '#top_up' do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    
+    it '#top_up will increase balance' do
+      expect { subject.top_up(10) }.to change { subject.balance }.by 10
     end
 
   end
