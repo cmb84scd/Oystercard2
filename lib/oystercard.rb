2f4e5dc8e8fdb28280a0e75fc1b121a1f9eb4632
HResalaty <Hmresalaty@yahoo.co.uk>
2020-04-21 16:08:13 +0100

    Refactor: in-line guard statement; passes

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,15 +2,16 @@ class Oystercard
   attr_reader :balance
 
   MAX_BALANCE = 90
-  
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    if @balance + amount >= MAX_BALANCE
-      raise 'Error: balance cannot be more than £90'
-    end
+    raise 'Error: balance cannot be more than £90' if @balance + amount >= MAX_BALANCE
+    # if @balance + amount >= MAX_BALANCE
+    #   raise 'Error: balance cannot be more than £90'
+    # end
     @balance += amount
   end
 
