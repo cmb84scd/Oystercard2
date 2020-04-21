48611e2d385253a77376f68300af90aa8d6263dd
HResalaty <Hmresalaty@yahoo.co.uk>
2020-04-21 16:05:25 +0100

    Adds MAXIMUM_BALANCE constant; passes

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
   attr_reader :balance
 
+  MAX_BALANCE = 90
+  
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    if @balance + amount >= 90
+    if @balance + amount >= MAX_BALANCE
       raise 'Error: balance cannot be more than £90'
     end
     @balance += amount
